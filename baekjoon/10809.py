ec724d194deb2554c3b2d5721129df59f12df16b
KIM 1412 <166794112+kms8032@users.noreply.github.com>
2025-08-02 15:26:28 +0900

    08/02 commit

--- baekjoon/10809.py
+++ baekjoon/10809.py
@@ -4,27 +4,25 @@ s = input("")
 # 알파벳을 나열
 alpabet = 'abcdefghijklmnopqrstuvwxyz'
 
-# 알파벳을 쪼개고 단어를 쪼개고 인덱스 값 저장
+# 각 알파벳을 처음 등장한 위치를 저장할 리스트들
 s_save = []
 alpabet_save=[]
-for i in range(len(alpabet)):
+
+for i in range(len(alpabet)): # 
     for j in range(len(s)):
-        if alpabet[i] == s[j]:
-            s_save.append(j)
-            alpabet_save.append(i)
+        if alpabet[i] == s[j]: # 현재 알파벳이 문자열에 있으면
+            if i not in alpabet_save: # 중복 제거
+                s_save.append(j)
+                alpabet_save.append(i)
 
-
-alpa = []
-for a in range(len(alpabet_save)):
-    alpa.append(alpabet_save[a])
-    if alpa[a] in alpa[:a]:
-        del alpa[a]
+# 초기값은 모두 -1로 채운 결과 리스트
 output = [-1] * len(alpabet)
 
+# 저장해둔 알파벳 인덱스를 기준으로, 그 알파벳이 처음 등장한 위치를 기록
 h = 0
-for k in alpa:
+for k in alpabet_save: # 저장해둔 알파벳 인덱스를 꺼내면서
+    output[k] = s_save[h] # 해당 알파벳 위치에 첫 등장 위치 저장
+    h +=1  # 1:1 매칭되도록 인덱스 동기화
 
-    output[k] = s_save[h]
-    h +=1 
-# 저장한 인덱스를 통해서 
-print(output)
+# 결과 출력 ( 리스트 형태가 아닌 공백으로 구분된 숫자 형태)
+print(" ".join(map(str, output)))
